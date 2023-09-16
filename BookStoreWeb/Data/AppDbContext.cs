665ba2c400bd546aaa1b66f6edff0ea2058bc1fe
IlyaM70 <iliya.merkulov@mail.ru>
2023-09-16 16:37:58 +0500

    Created Category Model Created DbContext Category table data seeded Created category controller

---
+++ BookStoreWeb/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+
+        public DbSet<Category> Categories  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "SciFi", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- BookStoreWeb/Program.cs
+++ BookStoreWeb/Program.cs
@@ -1,7 +1,13 @@
+using BookStoreWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PosgreSqlServer")));
+
 
 var app = builder.Build();
 
