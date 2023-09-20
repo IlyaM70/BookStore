5616b198e5ea505b1996ee673fe6f583428f3d4e
IlyaM70 <iliya.merkulov@mail.ru>
2023-09-20 16:03:25 +0500

    Home page and deatails page views done

--- BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.Models;
+﻿using BookStore.DataAccess.Repository.RepositoryInterface;
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,25 @@ namespace BookStore.Web.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products =
+                _unitOfWork.Product.GetAll(includeProperties: "Category"); 
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(p=>p.Id==id,includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
